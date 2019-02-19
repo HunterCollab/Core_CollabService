7d215a5f814402af2cd3985ac58184c5cd2e0fa6
Ram Vakada <ramv@Rams-MacBook-Pro.local>
2019-02-19 16:21:06 -0500

    /createUser Endpoint & gitignore updated

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask
-from AuthorizationAPI import auth_api
-from UserAPI import user_api
-
+from api.AuthorizationAPI import auth_api
+from api.UserAPI import user_api
 
 app = Flask(__name__)
 app.register_blueprint(user_api, url_prefix='/user')
@@ -10,7 +9,7 @@ app.register_blueprint(auth_api, url_prefix='/auth')
 
 @app.route("/")
 def helloWorld():
-  return "Hello World!"
+  return "Welcome to the Hunter Collab API. There's nothing to see here."
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
