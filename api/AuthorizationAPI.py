5073c12071b729f40ca69cf9a37495392eb79854
RamVakad <RamVakad@gmail.com>
2019-02-20 23:19:00 -0500

    Commit to master before branching.

--- api/AuthorizationAPI.py
+++ api/AuthorizationAPI.py
@@ -33,7 +33,6 @@ def login():
 SECRET_KEY = b'-\x1c\x9b\xa7x\xacH\nE{\x85=\xa6\x0e[\xe2\xe3\xb2\x01D\xc4\xd2x\x0f'
 #generates an encrypted auth token using the encrypted using the secret key valid for 24 hours
 def encode_auth_token(userName):
-    
     try:
         payload = {
             'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=1),
