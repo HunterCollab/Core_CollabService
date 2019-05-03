183050cdafacd246df89858ecd1085e2f27eaf87
RamVakad <RamVakad@gmail.com>
2019-05-03 17:52:06 -0400

    try catch on realtime handler

--- services/realtime/messaging/RMSClient.py
+++ services/realtime/messaging/RMSClient.py
@@ -13,35 +13,39 @@ class RMSClient(socketserver.BaseRequestHandler):
         super().__init__(request, client_address, server)
 
     def handle(self):
-        self.socket = self.request
-        self.addr = self.client_address[0]
+        try:
+            self.socket = self.request
+            self.addr = self.client_address[0]
 
-        self.log("New Session")
-        # The first message is the JWT token.
-        token = self.readNextMessage()
-        if token is None:
-            self.log("Session Ended PRE_HANDSHAKE")
-            return
+            self.log("New Session")
+            # The first message is the JWT token.
+            token = self.readNextMessage()
+            if token is None:
+                self.log("Session Ended PRE_HANDSHAKE")
+                return
 
-        self.log("Token Received: " + token)
+            self.log("Token Received: " + token)
 
-        username = decode_auth_token(token)
-        if username.startswith('SUCCESS'):
-            self.username = username[7:]
-            RealtimeServer.getInstance().addClient(self.username, self)
-            self.writeMessage("AUTH_SUCCESS")
-            self.log("Handshake Successful")
-            msg = self.readNextMessage()
-            while (msg):
-                if not msg == "DONE":
-                    msg = self.readNextMessage()
-        else:
-            self.writeMessage("AUTH_FAIL")
-            self.log("Invalid Auth - Closing Session")
-            return
+            username = decode_auth_token(token)
+            if username.startswith('SUCCESS'):
+                self.username = username[7:]
+                RealtimeServer.getInstance().addClient(self.username, self)
+                self.writeMessage("AUTH_SUCCESS")
+                self.log("Handshake Successful")
+                msg = self.readNextMessage()
+                while (msg):
+                    if not msg == "DONE":
+                        msg = self.readNextMessage()
+            else:
+                self.writeMessage("AUTH_FAIL")
+                self.log("Invalid Auth - Closing Session")
+                return
 
-        # just send back the same data, but upper-cased
-        self.writeMessage(token.upper())
+            # just send back the same data, but upper-cased
+            self.writeMessage(token.upper())
+        except Exception as e:
+            print(e)
+            self.log("EXCEPTION @ RMSClient.handle")
 
     def readNextMessage(self):
         return RMSProtocol.readUTF(self.socket)
