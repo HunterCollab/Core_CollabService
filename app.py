41bd8042eeb3668375b1514993deee81b2b97663
RamVakad <RamVakad@gmail.com>
2019-05-17 18:04:12 -0400

    More documenting

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.register_blueprint(user_api, url_prefix='/user')
-app.register_blueprint(auth_api, url_prefix='/auth')
-app.register_blueprint(collab_api, url_prefix='/collab')
-app.register_blueprint(search_api, url_prefix='/search')
-app.register_blueprint(messaging_api, url_prefix='/messaging')
+app.register_blueprint(user_api, url_prefix='/user') #All endpoints in UserAPI.py are prefixed with the /user route.
+app.register_blueprint(auth_api, url_prefix='/auth') #All endpoints in Authorization.py are prefixed with the /auth route.
+app.register_blueprint(collab_api, url_prefix='/collab') #All endpoints in CollaborationAPI.py are prefixed with the /collab route.
+app.register_blueprint(search_api, url_prefix='/search') #All endpoints in SearhAPI.py are prefixed with the /search route.
+app.register_blueprint(messaging_api, url_prefix='/messaging') #All endpoints in MessagingAPI.py are prefixed with the /messaging route.
 
 
+# Root
 @app.route("/", methods=['GET'])
 def helloWorld():
     return "Welcome to the Hunter Collab API. There's nothing to see here."
